9f7fe4050e5580d240a335f12635fca14279e97a
gandalfg <tasos.valtinos@gmail.com>
2025-11-24 13:50:23 +0000

    Added withdraw function my token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,12 +4,21 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// {} \ x / > < = > "" + & *×  # $ }{| /``@
+// Symbols Keyboard Broken
+// {} \ x / > < = > "" + & *×  # $ }{| /`` () ?  _ []
+// Contract Address after deployment
+// 
+
+
 /**
  * @title MyToken
  * @dev A simple ERC20 token contract with minting capabilities
  */
 contract MyToken is ERC20, Ownable {
     uint256 public constant INITIAL_SUPPLY = 1000000 * 10**18; // 1 million tokens with 18 decimals
+
+    event Withdraw(address indexed to, uint256 amount);
 
     constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, INITIAL_SUPPLY);
@@ -39,6 +48,13 @@ contract MyToken is ERC20, Ownable {
      */
     function getBalance(address account) public view returns (uint256) {
         return balanceOf(account);
+    }
+
+    function withdraw() public onlyOwner returns (uint256 amount) {
+        payable(owner()).transfer(address(this).balance);
+        emit Withdraw(owner(), address(this).balance);
+        amount = msg.sender.balance;
+        return amount;
     }
 }
 
